55b04af039208aee6685c05738da5e08daec1fc4
Bob <bob@liveramp.com>
2018-10-31 23:50:29 -0400

    finish tester interface

--- runner.go
+++ runner.go
@@ -384,8 +384,21 @@ func (c *Runner) fileLine(clr, text, filename string, line int, max int, args ..
 }
 
 type Tester interface {
+	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
+	Fail()
+	FailNow()
+	Failed() bool
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Log(args ...interface{})
+	Logf(format string, args ...interface{})
+	Name() string
 	Skip(args ...interface{})
+	SkipNow()
+	Skipf(format string, args ...interface{})
+	Skipped() bool
+	Helper()
 }
 
 type TestError struct {
